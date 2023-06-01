559720087526d790f2fb49c1ea90f1e6cd7ad0f6
Philipp Chapkovski <chapkovski@gmail.com>
2023-06-01 12:55:18 +0200

    rewriting api

--- exogeneous_model.py
+++ exogeneous_model.py
@@ -4,7 +4,7 @@ from scipy.stats import stats
 import sys
 import scipy as scipy
 from scipy import stats
-
+from update_book_nb import update_book_nb
 import scipy.stats as st
 
 import random
@@ -13,9 +13,7 @@ from scipy.stats import norm
 from datetime import datetime
 import statsmodels.api as sm
 import copy
-from numba import jit
-from numba import types, typed
-from numba.typed import List
+
 
 from numba.core.errors import NumbaDeprecationWarning, NumbaPendingDeprecationWarning
 import warnings
@@ -256,7 +254,20 @@ def create_limit_order_book(events, Ndepth, sigma=1, previous_mid_price=100.50, 
 
     while index < len(events) and event_time <= time_max:
         event_time, event_type, id_trader = events[index]
-
+        """
+        What we have here as an input:
+        1. event time
+        2. event_type
+        3. ask_side
+        4. bid_side
+        5. trade_history
+        6. flag_price_improvement
+        7. step_size
+        8. Ndepth
+        9. previous_mid_price
+        10. id_trader
+        """
+       
         ask_side, bid_side, trade_history, flag_price_improvement, passive_trader = update_book_nb(event_time,
                                                                                                    event_type, ask_side,
                                                                                                    bid_side,
@@ -319,37 +330,6 @@ def create_limit_order_book(events, Ndepth, sigma=1, previous_mid_price=100.50, 
             "agg_trader": agg_trader, 'pass_trader': pass_trader}
 
 
-# scroll the queue to the first free level
-@jit(nopython=True)
-def scroll_queue_update(side: np.ndarray, step_size: float, Ndepth: int, direction: int, id_trader: float) -> tuple:
-    # def scroll_queue_update(side, step_size, Ndepth, direction):
-    # direction 1 (ask), -1 (bid)
-    cond = True
-    iqueue = 1
-    while cond:
-
-        if iqueue == len(side):
-            cond = False
-            # create the new level
-            new_level = np.empty((1, 2+Ndepth))
-            new_level[0][0] = side[iqueue - 1][0] + step_size * direction
-            new_level[0][1] = id_trader
-            new_level[0][2] = 0
-            new_level[0][3:] = -1
-            side = np.vstack((side, new_level))
-        else:
-
-            if side[iqueue][1] + 1 > Ndepth:
-                iqueue = iqueue + 1  # try next level
-            else:
-                cond = False
-                side[iqueue][1] += 1
-                pos = side[iqueue][1]
-                side[iqueue][int(pos + 2 - 1)] = id_trader
-            # level with the first available spot
-
-    return iqueue, side
-
 
 def arrival_times_trader(Lambda=0.25, Number_events_trader=10):
     # U = 1 - e ^ {- Lambda X} -->  1 - U = e ^ {-Lambda X}  --> log(1 - U) = - Lambda X
@@ -362,230 +342,3 @@ def arrival_times_trader(Lambda=0.25, Number_events_trader=10):
     return events
 
 
-# @jit(nopython=True)
-def update_book_nb(event_time: float, event_type: int, ask_side: np.ndarray, bid_side: np.ndarray,
-                   trade_history: np.ndarray,
-                   flag_price_improvement: int, step_size: float, Ndepth: int, previous_mid_price: float,
-                   id_trader: float) -> tuple:
-    # event_type
-    #       0  aggressive buy
-    #       1  aggressive sell
-    #       2  passive buy  2.5 # buy with price improvements
-    #       3  passive sell 3.5 # sell with price improvements
-    #
-    # event type = -1 sell, 1 buy
-    # order_type = 0 market, 1 limit
-
-    # check if the book is empty
-    passive_id_trader = -1 # i.e., there is no passive traders
-    if len(bid_side) != 0 or len(ask_side) != 0:
-        if event_type == 0:
-            # Check if there are any sell orders to match
-
-            # Add trade to trade history
-            new_trade = np.array([(event_time, float(1), float(0), ask_side[0][0], event_type)])
-            trade_history = np.vstack((trade_history, new_trade))
-
-            # Remove matched order from sell book
-            ask_side[0][1] = ask_side[0][1] - 1
-            arr = ask_side[0].copy()
-            passive_id_trader = arr[2].copy()
-            arr[2:(-1)] = arr[3:]
-            arr[-1] = -1
-
-            ask_side[0] = arr
-
-            if ask_side[0][1] == 0:
-                ask_side = ask_side[1:]
-                flag_price_improvement = 'bid'
-
-        elif event_type == 1:
-
-            # Add trade to trade history
-            new_trade = np.array([(event_time, float(-1), float(0), bid_side[0][0], event_type)])
-            trade_history = np.vstack((trade_history, new_trade))
-
-            # Remove matched order from buy book
-            bid_side[0][1] = bid_side[0][1] - 1
-
-            arr = bid_side[0].copy()
-            passive_id_trader = arr[2].copy()
-            arr[2:(-1)] = arr[3:]
-            arr[-1] = -1
-
-            bid_side[0] = arr
-
-            if bid_side[0][1] == 0:
-                bid_side = bid_side[1:]
-                flag_price_improvement = 'ask'
-
-        elif event_type == 2:
-            # add the bid price
-
-            if flag_price_improvement == 'bid':
-
-                if bid_side[0][0] + step_size == ask_side[0][0]:
-                    #
-                    print('Spread is already closed with a previous price improvement in the bid')
-                    flag_price_improvement = None
-                    if (bid_side[0][1] + 1) > Ndepth:
-
-                        # update the queue in the next free level
-                        iqueue, bid_side = scroll_queue_update(bid_side, step_size, Ndepth, -1, id_trader)
-                        if bid_side[iqueue][0] < 0:
-                            # stack all the bid at last level
-                            bid_side = bid_side[:-1]
-                            iqueue = iqueue - 1
-                        new_trade = np.array([(event_time, float(1), float(1), bid_side[iqueue][0], event_type)])
-                        trade_history = np.vstack((trade_history, new_trade))
-
-                    else:
-                        bid_side[0][1] = bid_side[0][1] + 1
-                        pos = bid_side[0][1]
-                        bid_side[0][int(pos+2-1)] = id_trader
-
-                        new_trade = np.array([(event_time, float(1), float(1), bid_side[0][0], event_type)])
-                        trade_history = np.vstack((trade_history, new_trade))
-                else:
-
-                    # new_bid = list(np.empty((1, 2)))
-                    # new_bid[0][0] = bid_side[0][0] + step_size
-                    # new_bid[0][1] = 1
-                    #
-                    # bid_side = np.concatenate((new_bid, bid_side), axis=0)
-
-                    new_bid = np.empty((1, 2+Ndepth))
-                    new_bid[0][0] = bid_side[0][0] + step_size
-                    new_bid[0][1] = 1
-                    new_bid[0][2] = id_trader
-                    new_bid[0][3:] = -1
-                    bid_side = np.vstack((new_bid, bid_side))
-
-                    if bid_side[0][0] + step_size >= ask_side[0][0]:
-                        flag_price_improvement = None
-
-                    new_trade = np.array([(event_time, float(1), float(1), bid_side[0][0], 2.5)])
-                    trade_history = np.vstack((trade_history, new_trade))
-
-                # if (ask_side[0][0] - bid_side[0][0]) == 2*step_size:
-                # if P_a - P_b == spread then the bid is not improved, but this can not be happened, since
-                # if we are here, the top ask price was depleted, and the spread was opened
-            else:
-
-                if (bid_side[0][1] + 1) > Ndepth:
-
-                    # update the queue in the next free level
-
-                    iqueue, bid_side = scroll_queue_update(bid_side, step_size, Ndepth, -1, id_trader)
-                    if bid_side[iqueue][0] < 0:
-                        # stack all the bid at last level
-                        bid_side = bid_side[:-1]
-                        iqueue = iqueue - 1
-                    new_trade = np.array([(event_time, float(1), float(1), bid_side[iqueue][0], event_type)])
-                    trade_history = np.vstack((trade_history, new_trade))
-
-                else:
-                    bid_side[0][1] = bid_side[0][1] + 1
-
-                    pos = bid_side[0][1]
-                    bid_side[0][int(pos + 2 - 1)] = id_trader
-
-                    new_trade = np.array([(event_time, float(1), float(1), bid_side[0][0], event_type)])
-                    trade_history = np.vstack((trade_history, new_trade))
-            # Add trade to trade history
-        elif event_type == 3:
-
-            if flag_price_improvement == 'ask':
-
-                if ask_side[0][0] - step_size == bid_side[0][0]:
-                    print('Spread is already closed with a previous price improvement in the ask')
-                    flag_price_improvement = None
-                    if (ask_side[0][1] + 1) > Ndepth:
-
-                        # update the queue in the next free level
-
-                        iqueue, ask_side = scroll_queue_update(ask_side, step_size, Ndepth, 1, id_trader)
-                        new_trade = np.array([(event_time, float(-1), float(1), ask_side[iqueue][0], event_type)])
-                        trade_history = np.vstack((trade_history, new_trade))
-
-                    else:
-                        ask_side[0][1] = ask_side[0][1] + 1
-
-                        pos = ask_side[0][1]
-                        ask_side[0][int(pos + 2 - 1)] = id_trader
-
-                        new_trade = np.array([(event_time, float(-1), float(1), ask_side[0][0], event_type)])
-                        trade_history = np.vstack((trade_history, new_trade))
-                else:
-                    # new_ask = list(np.empty((1, 2)))
-                    # new_ask[0][0] = ask_side[0][0] - step_size
-                    # new_ask[0][1] = 1
-                    #
-                    # ask_side = np.concatenate((new_ask, ask_side), axis=0)
-
-                    new_ask = np.empty((1, 2+Ndepth))
-                    new_ask[0][0] = ask_side[0][0] - step_size
-                    new_ask[0][1] = 1
-                    new_ask[0][2] = id_trader
-                    new_ask[0][3:] = -1
-                    ask_side = np.vstack((new_ask, ask_side))
-
-                    if ask_side[0][0] - step_size <= bid_side[0][0]:
-                        flag_price_improvement = None
-
-                    new_trade = np.array([(event_time, float(-1), float(1), ask_side[0][0], 3.5)])
-                    trade_history = np.vstack((trade_history, new_trade))
-
-                # if (ask_side[0][0] - bid_side[0][0]) == 2*step_size:
-                # if P_a - P_b == spread then the ask_side is not improved, but this can not be happened, since
-                # if we are here, the top bid price was depleted, and the spread was opened
-            else:
-
-                if (ask_side[0][1] + 1) > Ndepth:
-
-                    # update the queue in the next free level
-
-                    iqueue, ask_side = scroll_queue_update(ask_side, step_size, Ndepth, 1, id_trader)
-                    new_trade = np.array([(event_time, float(-1), float(1), ask_side[iqueue][0], event_type)])
-                    trade_history = np.vstack((trade_history, new_trade))
-
-                else:
-                    ask_side[0][1] = ask_side[0][1] + 1
-
-                    pos = ask_side[0][1]
-                    ask_side[0][int(pos + 2 - 1)] = id_trader
-
-                    new_trade = np.array([(event_time, float(-1), float(1), ask_side[0][0], event_type)])
-                    trade_history = np.vstack((trade_history, new_trade))
-
-    else:
-        # check if the book is empty
-        if len(bid_side) == 0:
-            print("BID SIDE EMPTY")
-            if len(ask_side) == 0:
-                bid_price = previous_mid_price - step_size / 2
-            else:
-                bid_price = ask_side[0][0] - step_size
-
-            bid_volume = 1
-            bid_side = np.array([np.hstack(([bid_price, bid_volume, id_trader],
-                                 -1 * np.ones(Ndepth - 1)))], dtype=np.float64)
-
-            new_trade = np.array([(event_time, float(1), float(1), bid_side[0][0], 2.25)])
-            trade_history = np.vstack((trade_history, new_trade))
-
-        if len(ask_side) == 0:
-            print("ASK SIDE EMPTY")
-            if len(bid_side) == 0:
-                ask_price = previous_mid_price + step_size / 2
-            else:
-                ask_price = bid_side[0][0] + step_size
-
-            ask_volume = 1
-            ask_side = np.array([np.hstack(([ask_price, ask_volume, id_trader],
-                                            -1 * np.ones(Ndepth - 1)))], dtype=np.float64)
-
-            new_trade = np.array([(event_time, float(-1), float(1), ask_side[0][0], 3.25)])
-            trade_history = np.vstack((trade_history, new_trade))
-
-    return ask_side, bid_side, trade_history, flag_price_improvement, passive_id_trader
--- main.py
+++ main.py
@@ -7,97 +7,121 @@ import exogeneous_model as exog_fun
 import numpy as np
 import pandas as pd
 from beautifier import PrettyJSONResponse
+
 # Create an instance of the FastAPI class
 app = FastAPI()
 
 
 settings = {
     # 'hours': 60,  # minutes
-    'T_day': 60 * 6.5,  # mins in a trading day; trading day length in munutes
+    "T_day": 1,  # 60 * 6.5,  # mins in a trading day; trading day length in munutes
     # 60/3*0.5,  # 1/min   # 1 = 1min freq,  2  = a trading occurs every 30 sec, 0.5 = traders every 2 min
-    'freq_agg': 60/2*0.5,  # number of aggresive orders per minute
-    'freq_pass': 60/2,  # 60/3, number of passive orders per minute
-    'N_depth': 5,
-    'sigma': 100.0,  # price are multiplied by 100, 1/1e2 is one basis point
-    'previous_mid_price': 10000,
-    'seed': 2023,
+    "freq_agg": 1,  # 60/2*0.5,  # number of aggresive orders per minute
+    "freq_pass": 1,  # 60/2,  # 60/3, number of passive orders per minute
+    "N_depth": 5,
+    "sigma": 100.0,  # price are multiplied by 100, 1/1e2 is one basis point
+    "previous_mid_price": 10000,
+    "seed": 2023,
 }
 
 # Define a route for the root URL ("/") using the get() decorator
 
 
 @app.get("/", response_class=PrettyJSONResponse)
-async def read_root(request: Request,
-                    T_day: int = 60*6.5, freq_agg: int = 60/2*0.5,
-                    freq_pass: int = 60/2,
-                    N_depth: int = 5,
-                    sigma: int = 100,
-                    previous_mid_price: int = 10000,
-                    seed: int = 2023):
-
-    updated_params = dict(T_day=T_day,
-                          freq_agg=freq_agg,
-                          freq_pass=freq_pass,
-                          N_depth=N_depth,
-                          sigma=sigma,
-                          previous_mid_price=previous_mid_price,
-                          seed=seed
-                          )
-
-    new_settings = settings.copy()
-    new_settings.update(updated_params)
-    res = order_book_wrapper(new_settings)
+async def read_root(
+    request: Request,
+    position: int=0
+):
+    max_position=len(data)-1
+    day_over=position>=max_position
+        
+    position=min(position, max_position)
+    res = data[position].copy()
+    res['day_over']=day_over
     return res
 
 
 def order_book_wrapper(settings):
-    int_lambda = [settings['freq_agg'], settings['freq_agg'],
-                  settings['freq_pass'], settings['freq_pass']]
+    int_lambda = [
+        settings["freq_agg"],
+        settings["freq_agg"],
+        settings["freq_pass"],
+        settings["freq_pass"],
+    ]
 
     # create the exogenous order book
-    np.random.seed(seed=settings['seed'])
-    exogenous_events = exog_fun.create_event_times(J_traders=1,
-                                                   int_lambda=int_lambda,
-                                                   day=settings['T_day'])
+    np.random.seed(seed=settings["seed"])
+
+
+    settings["T_day"] = 10
+    exogenous_events = exog_fun.create_event_times(
+        J_traders=1, int_lambda=int_lambda, day=settings["T_day"]
+    )
 
     zeros_col = np.zeros((exogenous_events.shape[0], 1))
     event_book = np.column_stack((exogenous_events, zeros_col))
     # the last column is the idenity of the trader  1 is Algo
 
     # at each update of the book the algortihm check the state of the book
-    arg_input = {'inventory': 200,
-                 'granularity_frequency': 1/10000,
-                 'T_day': settings['T_day']}
+    arg_input = {
+        "inventory": 200,
+        "granularity_frequency": 1 / 10000,
+        "T_day": settings["T_day"],
+    }
 
-    out = exog_fun.create_limit_order_book(event_book, settings['N_depth'], settings['sigma'],
-                                           settings['previous_mid_price'], algo_trading=False, arg_input=arg_input)
+    out = exog_fun.create_limit_order_book(
+        event_book,
+        settings["N_depth"],
+        settings["sigma"],
+        settings["previous_mid_price"],
+        algo_trading=False,
+        arg_input=arg_input,
+    )
 
-    res = dict(settings=settings, pass_trader=dict(), agg_trader=dict(), data=list())
-    book_state_df = pd.DataFrame(out['book_state'])
+    res = dict(
+        settings=settings, pass_trader=dict(), agg_trader=dict(), data=list()
+    )
+    # our goal here is to glue together book_state, trade_history and trader_id
+    # the 
 
-    data = out['pass_trader'].copy()
+    def retrieve_book_state(_type):
+        res = []
 
-    data['cash_inventory_pass'] = data['cash_inventory_pass'].tolist()
-    data['inventory_pass'] = data['inventory_pass'].tolist()
+        for j, i in enumerate(out["book_state"]):
+            arr = i[0].get(_type)
+            arr_sub = arr[:, :3]
+            df = pd.DataFrame(arr_sub, columns=["timestamp", "action", "trader"])
+            res.append(df.to_dict(orient="records"))
 
-    res['pass_trader'] = data
-    data = out['agg_trader'].copy()
-    data['cash_inventory_agg'] = data['cash_inventory_agg'].tolist()
-    data['inventory_agg'] = data['inventory_agg'].tolist()
-    res['agg_trader'] = data
+        return res
 
+    bid_list = retrieve_book_state("bid")
+    ask_list = retrieve_book_state("ask")
     traders_identity_df = pd.DataFrame(
-        out['traders_identity'], columns=['trader_id'])
+        out["traders_identity"], columns=["trader_id"]
+    )
 
-    trade_history_df = pd.DataFrame(out['trade_history'], columns=[
-                                    'timestamp', 'direction', 'order_type', 'price', 'agression'])
+    trade_history_df = pd.DataFrame(
+        out["trade_history"],
+        columns=["timestamp", "direction", "order_type", "price", "agression"],
+    )
+    
     df = traders_identity_df.merge(
-        trade_history_df, left_index=True, right_index=True)
-    data = df.to_dict(orient='index')
-    res['data'] = data
-    return res
+        trade_history_df, left_index=True, right_index=True
+    )
+    trade_history = df.to_dict(orient="records")
+    increment_trade_history = [trade_history[:i+1] for i in range(len(trade_history))]
 
+    full_data_zip=zip(increment_trade_history, bid_list, ask_list)
+    full_data=[dict(
+        history=history, 
+        bid=bid,
+        ask=ask
+    ) for history, bid, ask in full_data_zip]
+    return full_data
+      
 
+data=order_book_wrapper(settings)
 # that we need for local testing of a wrapper
-if __name__ == '__main__':
-    order_book_wrapper(settings)
+if __name__ == "__main__":
+    print(data[0])
