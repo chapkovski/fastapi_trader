e0f4278475bc707a5c3e898fb26971ba9c42ae85
Philipp Chapkovski <chapkovski@gmail.com>
2023-06-02 11:35:28 +0200

    chartdata

--- main.py
+++ main.py
@@ -38,16 +38,13 @@ settings = {
 
 
 @app.get("/", response_class=PrettyJSONResponse)
-async def read_root(
-    request: Request,
-    position: int=0
-):
-    max_position=len(data)-1
-    day_over=position>=max_position
-        
-    position=min(position, max_position)
+async def read_root(request: Request, position: int = 0):
+    max_position = len(data) - 1
+    day_over = position >= max_position
+
+    position = min(position, max_position)
     res = data[position].copy()
-    res['day_over']=day_over
+    res["day_over"] = day_over
     return res
 
 
@@ -61,7 +58,6 @@ def order_book_wrapper(settings):
 
     # create the exogenous order book
     np.random.seed(seed=settings["seed"])
-
 
     settings["T_day"] = 10
     exogenous_events = exog_fun.create_event_times(
@@ -88,11 +84,9 @@ def order_book_wrapper(settings):
         arg_input=arg_input,
     )
 
-    res = dict(
-        settings=settings, pass_trader=dict(), agg_trader=dict(), data=list()
-    )
+    res = dict(settings=settings, pass_trader=dict(), agg_trader=dict(), data=list())
     # our goal here is to glue together book_state, trade_history and trader_id
-    # the 
+    # the
 
     def retrieve_book_state(_type):
         res = []
@@ -105,36 +99,59 @@ def order_book_wrapper(settings):
 
         return res
 
+    def generate_chart_prices(data, position):
+        types = ["bid", "ask"]
+        res=[]
+        for i in data[:position]:
+            temp_res = {}
+            for _type in types:
+                arr = i[0].get(_type)
+                arr_sub = arr[:, :3]
+                df = pd.DataFrame(arr_sub, columns=["price", "action", "trader"])
+                temp_res[_type] = df.price.mean()
+
+            temp_res["median"] = (temp_res["bid"] + temp_res["ask"]) / 2
+            res.append(temp_res)
+        ask_data=[i.get('ask') for i in res]
+        bid_data=[i.get('bid') for i in res]
+        median_data=[i.get('median') for i in res]
+        ask_series=dict(name='Ask', data=ask_data)
+        bid_series=dict(name='Bid', data=bid_data)
+        median_series=dict(name='Median', data=median_data)
+        res=[ask_series, bid_series, median_series]
+        return res
+
     bid_list = retrieve_book_state("bid")
     ask_list = retrieve_book_state("ask")
-    traders_identity_df = pd.DataFrame(
-        out["traders_identity"], columns=["trader_id"]
-    )
+    full_data_for_chart=[]
+    for i,j in enumerate(out["book_state"]):
+        data_for_chart=generate_chart_prices(out["book_state"], position=i)
+        full_data_for_chart.append(data_for_chart)
+    traders_identity_df = pd.DataFrame(out["traders_identity"], columns=["trader_id"])
 
     trade_history_df = pd.DataFrame(
         out["trade_history"],
         columns=["timestamp", "direction", "order_type", "price", "agression"],
     )
-    
-    df = traders_identity_df.merge(
-        trade_history_df, left_index=True, right_index=True
-    )
+
+    df = traders_identity_df.merge(trade_history_df, left_index=True, right_index=True)
     trade_history = df.to_dict(orient="records")
-    timestamps=df['timestamp'].tolist()
-    increment_trade_history = [trade_history[:i+1] for i in range(len(trade_history))]
+    timestamps = df["timestamp"].tolist()
+    increment_trade_history = [
+        trade_history[: i + 1] for i in range(len(trade_history))
+    ]
 
-    full_data_zip=zip(timestamps,increment_trade_history, bid_list, ask_list)
-    full_data=[dict(
-        timestamp=timestamp,
-        history=history, 
-        bid=bid,
-        ask=ask
-    ) for timestamp, history, bid, ask in full_data_zip]
-    
+    full_data_zip = zip(timestamps, increment_trade_history, bid_list, ask_list, full_data_for_chart)
+    full_data = [
+        dict(timestamp=timestamp, tot_length=len(timestamps), data_for_chart=data_for_chart,
+              history=history, bid=bid, ask=ask)
+        for timestamp, history, bid, ask, data_for_chart in full_data_zip
+    ]
+
     return full_data
-      
 
-data=order_book_wrapper(settings)
+
+data = order_book_wrapper(settings)
 # that we need for local testing of a wrapper
 if __name__ == "__main__":
     pass
